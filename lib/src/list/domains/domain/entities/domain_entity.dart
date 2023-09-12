cf019e33cb912dfb9b2bf129fd72f35b1f4c900d
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-13 02:59:56 +0800

    chore(entity): create domain entity

---
+++ lib/src/list/domains/domain/entities/domain_entity.dart
@@ -0,0 +1,32 @@
+import 'package:equatable/equatable.dart';
+
+/// Dart class representation of BPS Statistics Domain
+///
+/// docs: https://webapi.bps.go.id/documentation/#domain
+
+class DomainEntity extends Equatable {
+  /// Constructor of DomainEntity
+  const DomainEntity({
+    required this.id,
+    required this.name,
+    required this.url,
+  });
+
+  /// id of the domain
+  ///
+  /// represent 'domain_id' from api response
+  final String id;
+
+  /// name of the domain
+  ///
+  /// represent 'domain_name' from api response
+  final String name;
+
+  /// url of the domain
+  ///
+  /// represent 'domain_url' from api response
+  final String url;
+
+  @override
+  List<Object> get props => [];
+}
