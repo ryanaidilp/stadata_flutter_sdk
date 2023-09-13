aecf27b1b74a1398b5d5915284bf4c1fdf5c92b4
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-13 13:35:00 +0800

    chore(list): add publication to list

--- lib/src/list/list.dart
+++ lib/src/list/list.dart
@@ -2,21 +2,32 @@
 
 import 'package:injectable/injectable.dart';
 import 'package:stadata_flutter_sdk/src/core/di/service_locator.dart';
-import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
+import 'package:stadata_flutter_sdk/src/features/publications/domain/entities/publication.dart';
+import 'package:stadata_flutter_sdk/src/features/publications/domain/usecases/get_all_publication.dart';
 import 'package:stadata_flutter_sdk/src/list/domains/domain/usecases/get_domains.dart';
 import 'package:stadata_flutter_sdk/src/list/domains/domains.dart';
 import 'package:stadata_flutter_sdk/src/shared/domain/entities/pagination.dart';
+import 'package:stadata_flutter_sdk/src/shared/domain/enums/data_language.dart';
 
 abstract class StadataList {
   Future<ListResult<DomainEntity>> domains({
     DomainType type = DomainType.all,
     String? provinceCode,
   });
+  Future<ListResult<Publication>> publications({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+    String? keyword,
+    int? month,
+    int? year,
+  });
 }
 
 @LazySingleton(as: StadataList)
 class StadataListImpl implements StadataList {
   final getDomains = getIt<GetDomains>();
+  final getAllPublications = getIt<GetAllPublication>();
 
   @override
   Future<ListResult<DomainEntity>> domains({
@@ -31,12 +42,37 @@ class StadataListImpl implements StadataList {
     );
 
     return result.fold(
-      (l) => switch (l) {
-        DomainFailure(:final message) => throw Exception(message),
-        DomainProvinceCodeMissingFailure(:final message) =>
-          throw Exception(message),
-      },
+      (l) => throw Exception(l.message),
       (r) => ListResult<DomainEntity>(
+        data: r.data ?? [],
+        pagination: r.pagination,
+      ),
+    );
+  }
+
+  @override
+  Future<ListResult<Publication>> publications({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+    String? keyword,
+    int? month,
+    int? year,
+  }) async {
+    final result = await getAllPublications(
+      GetPublicationParam(
+        domain: domain,
+        lang: lang,
+        page: page,
+        keyword: keyword,
+        month: month,
+        year: year,
+      ),
+    );
+
+    return result.fold(
+      (l) => throw Exception(l.message),
+      (r) => ListResult(
         data: r.data ?? [],
         pagination: r.pagination,
       ),
