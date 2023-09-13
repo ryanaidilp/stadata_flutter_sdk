cadc316038dcd2a94533ab0f62b3c26156e1819d
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-13 13:30:49 +0800

    chore(enums): add new entry to data availability

--- lib/src/shared/domain/enums/data_availability.dart
+++ lib/src/shared/domain/enums/data_availability.dart
@@ -2,10 +2,12 @@
 
 enum DataAvailability {
   available,
+  notAvailable,
   listNotAvailable;
 
   String get value => switch (this) {
         available => 'available',
+        notAvailable => 'not-available',
         listNotAvailable => 'list-not-available',
       };
 }
