5e810b3216caa4abe756048a4ce9c077f0b1ed04
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-13 13:30:31 +0800

    chore(enums): add data language enums

---
+++ lib/src/shared/domain/enums/data_language.dart
@@ -0,0 +1,16 @@
+/// An enum representing different data languages.
+enum DataLanguage {
+  /// Indonesian language.
+  id,
+
+  /// English language.
+  en;
+
+  /// Gets the string representation of the enum value.
+  ///
+  /// Returns 'ind' for [DataLanguage.id] and 'eng' for [DataLanguage.en].
+  String get value => switch (this) {
+        id => 'ind',
+        en => 'eng',
+      };
+}
