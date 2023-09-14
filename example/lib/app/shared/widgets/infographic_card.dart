a776d9f34ec127b8bf4cfb33cca144aa437669fd
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-14 13:39:27 +0800

    feat(widget): add infographic card widget

---
+++ example/lib/app/shared/widgets/infographic_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:stadata_example/app/shared/widgets/network_image.dart';
+
+class InfographicCard extends StatelessWidget {
+  const InfographicCard({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.description,
+  });
+
+  final String title;
+
+  final String image;
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) => Stack(
+        children: [
+          AppNetworkImage(
+            url: image,
+            width: 1.sw,
+            height: 0.6.sh,
+          ),
+          Positioned(
+            bottom: 1,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 0.1.sh,
+              padding: EdgeInsets.all(8.r),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(8.r),
+                ),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black54,
+                    Colors.black,
+                  ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Colors.white,
+                        ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    description.isEmpty ? '---' : description,
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Colors.white,
+                        ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+}
