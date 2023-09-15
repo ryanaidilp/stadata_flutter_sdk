07e3a794c7c4e01debfe80f17518a16a314db741
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-15 12:01:09 +0800

    refactor(export): optimize file export

--- lib/src/features/domains/domains.dart
+++ lib/src/features/domains/domains.dart
@@ -1,2 +1,2 @@
-export 'package:stadata_flutter_sdk/src/features/domains/domain/entities/domain_entity.dart';
-export 'package:stadata_flutter_sdk/src/features/domains/domain/enums/domain_type.dart';
+export './domain/entities/domain_entity.dart';
+export './domain/enums/domain_type.dart';
