01551ba9c5d968819f6e1ce00d526fab2739325f
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-16 12:57:28 +0800

    feat(new_categories): add news categories to features

--- lib/src/features/features.dart
+++ lib/src/features/features.dart
@@ -1,5 +1,6 @@
 export './domains/domains.dart';
 export './infographics/infographics.dart';
-export './news/domain/entities/news.dart';
+export './news/news.dart';
+export './news_categories/news_categories.dart';
 export './publications/publications.dart';
 export './static_tables/static_tables.dart';
