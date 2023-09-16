f482101771368ef57deae16c52285e98e98bd3c1
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-16 12:07:03 +0800

    feat(repository): create news category repository contract

---
+++ lib/src/features/news_category/domain/respositories/news_category_repository.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:dartz/dartz.dart';
+import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
+import 'package:stadata_flutter_sdk/src/features/news_category/domain/entities/news_category.dart';
+import 'package:stadata_flutter_sdk/src/shared/domain/entities/api_response.dart';
+import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
+
+abstract class NewsCategoryRepository {
+  Future<Either<Failure, ApiResponse<List<NewsCategory>>>> get({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+  });
+}
