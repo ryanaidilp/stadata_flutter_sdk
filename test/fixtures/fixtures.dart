a729ebdd607c6d6a412a0021699301679f3d252f
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-16 08:04:34 +0800

    test(fixtures): create fixture enums

--- test/fixtures/fixtures.dart
+++ test/fixtures/fixtures.dart
@@ -1,6 +1,24 @@
 import 'dart:convert';
 import 'dart:io';
 
+enum Fixture {
+  domains('domain_fixture_available.json'),
+  error('fixture_error.json'),
+  infographics('infographic_fixture_available.json'),
+  listUnavailable('fixture_list_unavailable.json'),
+  news('news_fixture_available.json'),
+  newsDetail('news_detail_fixture.json'),
+  publications('publication_fixture_available.json'),
+  publicationDetail('publication_detail_fixture.json'),
+  staticTables('static_table_fixture_available.json'),
+  staticTableDetail('static_table_detail_fixture.json'),
+  unavailable('fixture_unavailable.json');
+
+  const Fixture(this.value);
+
+  final String value;
+}
+
 String fixture(String name) => File('test/fixtures/$name').readAsStringSync();
 
 Map<String, dynamic> jsonFromFixture(String name) {
