4b2c9b7750fc05e747f65ea153000f5c3a0ca8f6
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-09-25 13:55:33 +0800

    feat(list): add subject categories, subjects, & press releases

--- lib/src/list/list.dart
+++ lib/src/list/list.dart
@@ -6,8 +6,11 @@ import 'package:stadata_flutter_sdk/src/features/domains/domain/usecases/get_dom
 import 'package:stadata_flutter_sdk/src/features/infographics/domain/usecases/get_all_infographics.dart';
 import 'package:stadata_flutter_sdk/src/features/news/domain/usecases/get_all_news.dart';
 import 'package:stadata_flutter_sdk/src/features/news_categories/domain/usecases/get_all_news_categories.dart';
+import 'package:stadata_flutter_sdk/src/features/press_releases/domain/usecases/get_all_press_releases.dart';
 import 'package:stadata_flutter_sdk/src/features/publications/domain/usecases/get_all_publication.dart';
 import 'package:stadata_flutter_sdk/src/features/static_tables/domain/usecases/get_all_static_tables.dart';
+import 'package:stadata_flutter_sdk/src/features/subject_categories/domain/usecases/get_all_subject_categories.dart';
+import 'package:stadata_flutter_sdk/src/features/subjects/domain/usecases/get_all_subjects.dart';
 import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
 
 abstract class StadataList {
@@ -55,6 +58,28 @@ abstract class StadataList {
     required String domain,
     DataLanguage lang = DataLanguage.id,
   });
+
+  Future<ListResult<SubjectCategory>> subjectCategories({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+  });
+
+  Future<ListResult<Subject>> subjects({
+    required String domain,
+    required int subjectCategoryId,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+  });
+
+  Future<ListResult<PressRelease>> pressReleases({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+    String? keyword,
+    int? month,
+    int? year,
+  });
 }
 
 @LazySingleton(as: StadataList)
@@ -65,6 +90,9 @@ class StadataListImpl implements StadataList {
   final _getAllStaticTables = getIt<GetAllStaticTables>();
   final _getAllNews = getIt<GetAllNews>();
   final _getAllNewsCategories = getIt<GetAllNewsCategories>();
+  final _getAllSubjectCategories = getIt<GetAllSubjectCategories>();
+  final _getAllSubjects = getIt<GetAllSubjects>();
+  final _getAllPressReleases = getIt<GetAllPressReleases>();
 
   @override
   Future<ListResult<DomainEntity>> domains({
@@ -216,6 +244,84 @@ class StadataListImpl implements StadataList {
     return result.fold(
       (l) => throw NewsCategoryException(message: l.message),
       (r) => ListResult<NewsCategory>(
+        data: r.data ?? [],
+        pagination: r.pagination,
+      ),
+    );
+  }
+
+  @override
+  Future<ListResult<SubjectCategory>> subjectCategories({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+  }) async {
+    final result = await _getAllSubjectCategories(
+      GetAllSubjectCategoriesParam(
+        lang: lang,
+        page: page,
+        domain: domain,
+      ),
+    );
+
+    return result.fold(
+      (l) => throw SubjectCategoryException(
+        message: l.message,
+      ),
+      (r) => ListResult<SubjectCategory>(
+        data: r.data ?? [],
+        pagination: r.pagination,
+      ),
+    );
+  }
+
+  @override
+  Future<ListResult<Subject>> subjects({
+    required String domain,
+    required int subjectCategoryId,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+  }) async {
+    final result = await _getAllSubjects(
+      GetAllSubjectsParam(
+        page: page,
+        lang: lang,
+        domain: domain,
+        subjectCategoryId: subjectCategoryId,
+      ),
+    );
+
+    return result.fold(
+      (l) => throw SubjectException(message: l.message),
+      (r) => ListResult<Subject>(
+        data: r.data ?? [],
+        pagination: r.pagination,
+      ),
+    );
+  }
+
+  @override
+  Future<ListResult<PressRelease>> pressReleases({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+    String? keyword,
+    int? month,
+    int? year,
+  }) async {
+    final result = await _getAllPressReleases(
+      GetAllPressReleasesParam(
+        page: page,
+        year: year,
+        month: month,
+        domain: domain,
+        keyword: keyword,
+      ),
+    );
+
+    return result.fold(
+      (l) => throw PressReleaseException(message: l.message),
+      (r) => ListResult<PressRelease>(
         data: r.data ?? [],
         pagination: r.pagination,
       ),
