af8c67139d2aeefc2b1aba907dc53aec67a70805
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-06 18:43:25 +0800

    test(list): add unit test for strategicIndicators method

--- test/src/list/list_test.dart
+++ test/src/list/list_test.dart
@@ -17,6 +17,8 @@ import 'package:stadata_flutter_sdk/src/features/publications/data/models/public
 import 'package:stadata_flutter_sdk/src/features/publications/domain/usecases/get_all_publication.dart';
 import 'package:stadata_flutter_sdk/src/features/static_tables/data/models/static_table_model.dart';
 import 'package:stadata_flutter_sdk/src/features/static_tables/domain/usecases/get_all_static_tables.dart';
+import 'package:stadata_flutter_sdk/src/features/strategic_indicators/data/models/strategic_indicator_model.dart';
+import 'package:stadata_flutter_sdk/src/features/strategic_indicators/domain/usecases/get_all_strategic_indicators.dart';
 import 'package:stadata_flutter_sdk/src/features/subject_categories/data/models/subject_category_model.dart';
 import 'package:stadata_flutter_sdk/src/features/subject_categories/domain/usecases/get_all_subject_categories.dart';
 import 'package:stadata_flutter_sdk/src/features/subjects/data/models/subject_model.dart';
@@ -49,6 +51,9 @@ class MockGetAllSubjects extends Mock implements GetAllSubjects {}
 
 class MockGetAllPressReleases extends Mock implements GetAllPressReleases {}
 
+class MockGetAllStrategicIndicators extends Mock
+    implements GetAllStrategicIndicators {}
+
 void main() {
   late GetAllNews mockGetAllNews;
   late GetDomains mockGetDomains;
@@ -59,6 +64,7 @@ void main() {
   late GetAllSubjectCategories mockGetAllSubjectCategories;
   late GetAllSubjects mockGetAllSubjects;
   late GetAllPressReleases mockGetAllPressReleases;
+  late GetAllStrategicIndicators mockGetAllStrategicIndicators;
   late StadataList stadataList;
 
   setUpAll(
@@ -83,6 +89,10 @@ void main() {
       registerTestLazySingleton<GetAllSubjects>(mockGetAllSubjects);
       mockGetAllPressReleases = MockGetAllPressReleases();
       registerTestLazySingleton<GetAllPressReleases>(mockGetAllPressReleases);
+      mockGetAllStrategicIndicators = MockGetAllStrategicIndicators();
+      registerTestLazySingleton<GetAllStrategicIndicators>(
+        mockGetAllStrategicIndicators,
+      );
       stadataList = StadataListImpl();
     },
   );
@@ -922,6 +932,101 @@ void main() {
               verify(
                 () => mockGetAllPressReleases(
                   const GetAllPressReleasesParam(domain: domain),
+                ),
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'strategicIndicators()',
+        () {
+          late ApiResponse<List<StrategicIndicator>> response;
+          late ListResult<StrategicIndicator> data;
+
+          setUp(
+            () {
+              final json = jsonFromFixture(Fixture.strategicIndicators);
+              final jsonResponse =
+                  ApiResponseModel<List<StrategicIndicatorModel>>.fromJson(
+                json,
+                (json) {
+                  if (json is! List) {
+                    return [];
+                  }
+
+                  return json
+                      .map((e) => StrategicIndicatorModel.fromJson(e as JSON))
+                      .toList();
+                },
+              );
+              final responseData =
+                  jsonResponse.data?.map((e) => e.toEntity()).toList() ?? [];
+              response = ApiResponse(
+                data: responseData,
+                status: jsonResponse.status,
+                dataAvailability: jsonResponse.dataAvailability,
+                message: jsonResponse.message,
+                pagination: jsonResponse.pagination?.toEntity(),
+              );
+              data = ListResult<StrategicIndicator>(
+                data: responseData,
+                dataAvailability: response.dataAvailability ??
+                    DataAvailability.listNotAvailable,
+                pagination: response.pagination,
+              );
+            },
+          );
+          test(
+            'should return ListResult<StrategicIndicator> when success',
+            () async {
+              when(
+                () => mockGetAllStrategicIndicators(
+                  const GetAllStrategicIndicatorsParam(domain: domain),
+                ),
+              ).thenAnswer((_) async => Right(response));
+
+              final result = await stadataList.strategicIndicators(
+                domain: domain,
+              );
+
+              expect(result, data);
+              verify(
+                () => mockGetAllStrategicIndicators(
+                  const GetAllStrategicIndicatorsParam(domain: domain),
+                ),
+              );
+            },
+          );
+
+          test(
+            'should throw Exception if failure occured',
+            () async {
+              when(
+                () => mockGetAllStrategicIndicators(
+                  const GetAllStrategicIndicatorsParam(domain: domain),
+                ),
+              ).thenAnswer(
+                (_) async => const Left(
+                  StrategicIndicatorFailure(),
+                ),
+              );
+
+              expect(
+                () => stadataList.strategicIndicators(domain: domain),
+                throwsA(
+                  isA<Exception>().having(
+                    (e) => e.toString(),
+                    'Exception message',
+                    'StadataException - Failed to load '
+                        'strategic indicator data!',
+                  ),
+                ),
+              );
+              verify(
+                () => mockGetAllStrategicIndicators(
+                  const GetAllStrategicIndicatorsParam(domain: domain),
                 ),
               );
             },
