c286b523af8c71c5abc6c9c4aafe1b5778cc1da6
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-20 18:47:23 +0800

    feat(models): create variable model class

---
+++ lib/src/features/variables/data/models/variable_model.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:stadata_flutter_sdk/src/features/variables/domain/entity/variable.dart';
+
+part 'variable_model.freezed.dart';
+part 'variable_model.g.dart';
+
+@freezed
+abstract class VariableModel with _$VariableModel {
+  factory VariableModel({
+    @JsonKey(name: 'var_id') required int id,
+    @JsonKey() required String graphName,
+    @JsonKey() required String notes,
+    @JsonKey(name: 'sub_id') required int subjectID,
+    @JsonKey(name: 'sub_name') required String subjectName,
+    @JsonKey() required String title,
+    @JsonKey() required String unit,
+    @JsonKey(name: 'vertical') required int verticalVariableID,
+    @JsonKey(name: 'subcsa_id') int? csaSubjectID,
+    @JsonKey(name: 'subcsa_name') String? csaSubjectName,
+    @JsonKey() int? type,
+    @JsonKey(name: 'turth') int? derivedPeriodID,
+    @JsonKey(name: 'turvar') int? derivedVariableID,
+  }) = _VariableModel;
+  factory VariableModel.fromJson(Map<String, dynamic> json) =>
+      _$VariableModelFromJson(json);
+}
+
+extension VariableModelX on VariableModel {
+  Variable toEntity() => Variable(
+        id: id,
+        graphName: graphName,
+        notes: notes,
+        subjectID: subjectID,
+        subjectName: subjectName,
+        title: title,
+        unit: unit,
+        verticalVariableID: verticalVariableID,
+        type: type,
+        csaSubjectID: csaSubjectID,
+        csaSubjectName: csaSubjectName,
+        derivedPeriodID: derivedPeriodID,
+        derivedVariableID: derivedVariableID,
+      );
+}
