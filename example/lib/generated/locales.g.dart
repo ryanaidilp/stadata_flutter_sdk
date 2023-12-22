c9ab42824c4ffe886b084c385933d7f9f237408e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-22 21:46:13 +0800

    feat(locales): create locales for example app

---
+++ example/lib/generated/locales.g.dart
@@ -0,0 +1,109 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars, constant_identifier_names
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'id_ID': Locales.id_ID,
+    'en_US': Locales.en_US,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const stadata_example = 'stadata_example';
+  static const features_domains = 'features_domains';
+  static const features_infographics = 'features_infographics';
+  static const features_list = 'features_list';
+  static const features_news = 'features_news';
+  static const features_news_categories = 'features_news_categories';
+  static const features_press_releases = 'features_press_releases';
+  static const features_publications = 'features_publications';
+  static const features_static_tables = 'features_static_tables';
+  static const features_strategic_indicators = 'features_strategic_indicators';
+  static const features_subject_categories = 'features_subject_categories';
+  static const features_subjects = 'features_subjects';
+  static const features_variables = 'features_variables';
+  static const page_domain = 'page_domain';
+  static const label_empty = 'label_empty';
+  static const label_result = 'label_result';
+  static const label_custom_param = 'label_custom_param';
+  static const label_type = 'label_type';
+  static const label_province_code = 'label_province_code';
+  static const label_pagination_main = 'label_pagination_main';
+  static const label_pagination_page = 'label_pagination_page';
+  static const label_pagination_pages = 'label_pagination_pages';
+  static const label_pagination_per_page = 'label_pagination_per_page';
+  static const label_pagination_total = 'label_pagination_total';
+  static const button_submit = 'button_submit';
+  static const enums_domain_all = 'enums_domain_all';
+  static const enums_domain_province = 'enums_domain_province';
+  static const enums_domain_regency = 'enums_domain_regency';
+  static const enums_domain_regency_by_province =
+      'enums_domain_regency_by_province';
+}
+
+class Locales {
+  static const id_ID = {
+    'stadata_example': 'Contoh Stadata',
+    'features_domains': 'Domain',
+    'features_infographics': 'Infografis',
+    'features_list': 'Daftar',
+    'features_news': 'Berita',
+    'features_news_categories': 'Kategori Berita',
+    'features_press_releases': 'Press Rilis',
+    'features_publications': 'Publikasi',
+    'features_static_tables': 'Tabel Statis',
+    'features_strategic_indicators': 'Indikator Strategis',
+    'features_subject_categories': 'Kategori Subjek',
+    'features_subjects': 'Subjek',
+    'features_variables': 'Variabel',
+    'page_domain': 'Halaman Domain',
+    'label_empty': 'Kosong',
+    'label_result': 'Hasil',
+    'label_custom_param': 'Kustomisasi Parameter',
+    'label_type': 'Jenis (type) - wajib',
+    'label_province_code': 'Kode Provinsi (prov) - opsional',
+    'label_pagination_main': 'Data Halaman',
+    'label_pagination_page': 'Halaman: @page',
+    'label_pagination_pages': 'Jumlah Halaman: @pages',
+    'label_pagination_per_page': 'Per Halaman: @per_page',
+    'label_pagination_total': 'Total: @total',
+    'button_submit': 'Kirim',
+    'enums_domain_all': 'Semua (all)',
+    'enums_domain_province': 'Provinsi (province)',
+    'enums_domain_regency': 'Kabupaten (regency)',
+    'enums_domain_regency_by_province': 'Kabupaten by Provinsi (regencyByProv)',
+  };
+  static const en_US = {
+    'stadata_example': 'Stadata Example',
+    'features_domains': 'Domains',
+    'features_infographics': 'Infographics',
+    'features_list': 'List',
+    'features_news': 'News',
+    'features_news_categories': 'News Categories',
+    'features_press_releases': 'Press Releases',
+    'features_publications': 'Publications',
+    'features_static_tables': 'Static Tables',
+    'features_strategic_indicators': 'Strategic Indicators',
+    'features_subject_categories': 'Subject Categories',
+    'features_subjects': 'Subjects',
+    'features_variables': 'Variables',
+    'page_domain': 'Domain Page',
+    'label_empty': 'Empty',
+    'label_result': 'Result',
+    'label_custom_param': 'Custom Param',
+    'label_type': 'Type (type) - required',
+    'label_province_code': 'Province Code (prov) - optional',
+    'label_pagination_main': 'Pagination',
+    'label_pagination_page': 'Page: @page',
+    'label_pagination_pages': 'Pages: @pages',
+    'label_pagination_per_page': 'Per Page: @per_page',
+    'label_pagination_total': 'Total: @total',
+    'button_submit': 'Submit',
+    'enums_domain_all': 'All (all)',
+    'enums_domain_province': 'Province (province)',
+    'enums_domain_regency': 'Regency (regency)',
+    'enums_domain_regency_by_province': 'Regency by Province (regencyByProv)',
+  };
+}
