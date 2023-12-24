7903fdc7b0f83801c320d4524d49c1b4de69ccbc
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-24 13:23:12 +0800

    refactor(home): add navigation to home page

--- example/lib/app/modules/home/views/home_view.dart
+++ example/lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 
 import 'package:get/get.dart';
 import 'package:stadata_example/app/routes/app_pages.dart';
+import 'package:stadata_example/generated/locales.g.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -12,27 +14,56 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Stadata Example'),
+        title: Text(
+          LocaleKeys.stadata_example.tr,
+        ),
         centerTitle: true,
+        actions: [
+          ValueBuilder<Locale?>(
+            initialValue: Get.deviceLocale,
+            builder: (snapshot, updater) => FlutterSwitch(
+              value: snapshot == const Locale('id', 'ID'),
+              showOnOff: true,
+              onToggle: (value) {
+                final locale =
+                    value ? const Locale('id', 'ID') : const Locale('en', 'US');
+                Get.updateLocale(locale);
+                updater.call(locale);
+              },
+              activeText: 'ID',
+              inactiveText: 'EN',
+            ),
+          )
+        ],
       ),
       body: ListView(
         padding: EdgeInsets.all(20.r),
         children: [
           Text(
-            'List',
+            LocaleKeys.features_list.tr,
             style: Theme.of(context).textTheme.titleLarge,
           ),
           8.verticalSpace,
-          const _Button('Domains', Routes.DOMAIN),
-          const _Button('Infographics', Routes.INFOGRAPHIC),
-          const _Button('News', Routes.NEWS),
-          const _Button('News Categories', Routes.NEWS_CATEGORY),
-          const _Button('Press Releases', Routes.PRESS_RELEASE),
-          const _Button('Publications', Routes.PUBLICATION),
-          const _Button('Static Tables', Routes.STATIC_TABLE),
-          const _Button('Strategic Indicators', Routes.STRATEGIC_INDICATOR),
-          const _Button('Subject Categories', Routes.SUBJECT_CATEGORY),
-          const _Button('Subjects', Routes.SUBJECT),
+          _Button(LocaleKeys.features_domains.tr, Routes.DOMAIN),
+          _Button(LocaleKeys.features_infographics.tr, Routes.INFOGRAPHIC),
+          _Button(LocaleKeys.features_news.tr, Routes.NEWS),
+          _Button(
+            LocaleKeys.features_news_categories.tr,
+            Routes.NEWS_CATEGORY,
+          ),
+          _Button(LocaleKeys.features_press_releases.tr, Routes.PRESS_RELEASE),
+          _Button(LocaleKeys.features_publications.tr, Routes.PUBLICATION),
+          _Button(LocaleKeys.features_static_tables.tr, Routes.STATIC_TABLE),
+          _Button(
+            LocaleKeys.features_strategic_indicators.tr,
+            Routes.STRATEGIC_INDICATOR,
+          ),
+          _Button(
+            LocaleKeys.features_subject_categories.tr,
+            Routes.SUBJECT_CATEGORY,
+          ),
+          _Button(LocaleKeys.features_subjects.tr, Routes.SUBJECT),
+          _Button(LocaleKeys.features_variables.tr, Routes.VARIABLE),
         ],
       ),
     );
