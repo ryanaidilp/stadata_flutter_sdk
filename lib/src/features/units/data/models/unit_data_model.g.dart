fe2e9126610d73cefd8594d708e27ce8bfd153ee
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-25 21:04:45 +0800

    feat(models): create & generate unit data model

---
+++ lib/src/features/units/data/models/unit_data_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'unit_data_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UnitDataModelImpl _$$UnitDataModelImplFromJson(Map<String, dynamic> json) =>
+    _$UnitDataModelImpl(
+      id: json['unit_id'] as int,
+      title: json['unit'] as String,
+    );
+
+Map<String, dynamic> _$$UnitDataModelImplToJson(_$UnitDataModelImpl instance) =>
+    <String, dynamic>{
+      'unit_id': instance.id,
+      'unit': instance.title,
+    };
