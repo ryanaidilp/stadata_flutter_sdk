6cbe42d0802fc4ac143afe92363d75991dfdee2e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-12-25 21:02:20 +0800

    feat(repository-domain): create unit data repository interface

---
+++ lib/src/features/units/domain/repositories/unit_data_repository.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:dartz/dartz.dart';
+import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
+import 'package:stadata_flutter_sdk/src/shared/domain/entities/api_response.dart';
+import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
+
+abstract class UnitDataRepository {
+  Future<Either<Failure, ApiResponse<List<UnitData>>>> get({
+    required String domain,
+    DataLanguage lang = DataLanguage.id,
+    int page = 1,
+    int? variableID,
+  });
+}
