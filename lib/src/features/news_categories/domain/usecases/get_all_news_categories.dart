726c051bfd0ed606a2043394a147ec54d12a8ed9
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-04-28 01:13:09 +0800

    refactor(news_categories): update news category feature dependency injection

--- lib/src/features/news_categories/domain/usecases/get_all_news_categories.dart
+++ lib/src/features/news_categories/domain/usecases/get_all_news_categories.dart
@@ -2,16 +2,15 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:injectable/injectable.dart';
+
 import 'package:stadata_flutter_sdk/src/base/usecase.dart';
-import 'package:stadata_flutter_sdk/src/core/di/service_locator.dart';
+import 'package:stadata_flutter_sdk/src/core/di/injector.dart';
 import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
 import 'package:stadata_flutter_sdk/src/features/news_categories/domain/respositories/news_category_repository.dart';
 import 'package:stadata_flutter_sdk/src/shared/domain/entities/api_response.dart';
 
 import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
 
-@LazySingleton()
 class GetAllNewsCategories
     implements
         UseCase<ApiResponse<List<NewsCategory>>, GetAllNewsCategoriesParam,
@@ -26,7 +25,7 @@ class GetAllNewsCategories
       );
 
   @override
-  NewsCategoryRepository get repo => getIt<NewsCategoryRepository>();
+  NewsCategoryRepository get repo => injector.get<NewsCategoryRepository>();
 }
 
 class GetAllNewsCategoriesParam extends Equatable {
