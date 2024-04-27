68772f974bc0e4f153facf0dfbc97e34b4ea91f0
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-04-28 01:15:52 +0800

    refactor(subject): update subject feature dependency injection

--- lib/src/features/subjects/data/repositories/subject_repository_impl.dart
+++ lib/src/features/subjects/data/repositories/subject_repository_impl.dart
@@ -3,8 +3,8 @@
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
-import 'package:injectable/injectable.dart';
-import 'package:stadata_flutter_sdk/src/core/di/service_locator.dart';
+
+import 'package:stadata_flutter_sdk/src/core/di/injector.dart';
 import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
 import 'package:stadata_flutter_sdk/src/features/subjects/data/datasources/subject_remote_data_source.dart';
 import 'package:stadata_flutter_sdk/src/features/subjects/data/models/subject_model.dart';
@@ -15,9 +15,8 @@ import 'package:stadata_flutter_sdk/src/shared/domain/entities/api_response.dart
 import 'package:stadata_flutter_sdk/src/shared/domain/enums/data_language.dart';
 import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
 
-@LazySingleton(as: SubjectRepository)
 class SubjectRepositoryImpl implements SubjectRepository {
-  final _remoteDataSource = getIt<SubjectRemoteDataSource>();
+  final _remoteDataSource = injector.get<SubjectRemoteDataSource>();
   @override
   Future<Either<Failure, ApiResponse<List<Subject>>>> get({
     required String domain,
--- lib/src/features/subjects/domain/usecases/get_all_subjects.dart
+++ lib/src/features/subjects/domain/usecases/get_all_subjects.dart
@@ -2,16 +2,15 @@
 
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:injectable/injectable.dart';
+
 import 'package:stadata_flutter_sdk/src/base/usecase.dart';
-import 'package:stadata_flutter_sdk/src/core/di/service_locator.dart';
+import 'package:stadata_flutter_sdk/src/core/di/injector.dart';
 import 'package:stadata_flutter_sdk/src/core/failures/failures.dart';
 import 'package:stadata_flutter_sdk/src/features/subjects/domain/repositories/subject_repository.dart';
 import 'package:stadata_flutter_sdk/src/shared/domain/entities/api_response.dart';
 
 import 'package:stadata_flutter_sdk/stadata_flutter_sdk.dart';
 
-@LazySingleton()
 class GetAllSubjects
     implements
         UseCase<ApiResponse<List<Subject>>, GetAllSubjectsParam,
@@ -28,7 +27,7 @@ class GetAllSubjects
       );
 
   @override
-  SubjectRepository get repo => getIt<SubjectRepository>();
+  SubjectRepository get repo => injector.get<SubjectRepository>();
 }
 
 class GetAllSubjectsParam extends Equatable {
