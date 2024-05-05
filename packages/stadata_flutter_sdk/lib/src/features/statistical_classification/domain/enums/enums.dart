06b3d2ac84f6c2f54dd89504b7eab134b1c855ae
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-05-05 13:34:19 +0800

    refactor(barrel): rename and sort class

--- packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/enums/enums.dart
+++ packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/enums/enums.dart
@@ -1,6 +1,6 @@
+export 'classification_level.dart';
+export 'classification_type.dart';
 export 'kbki_level.dart';
 export 'kbki_type.dart';
 export 'kbli_level.dart';
 export 'kbli_type.dart';
-export 'statistical_classification.dart';
-export 'statistical_classification_level.dart';
