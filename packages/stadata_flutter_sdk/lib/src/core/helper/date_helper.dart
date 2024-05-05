1daba6753d403b6a0b93917ec34365479c11b817
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-05-05 18:42:40 +0800

    feat(helper): create date helper

---
+++ packages/stadata_flutter_sdk/lib/src/core/helper/date_helper.dart
@@ -0,0 +1,10 @@
+class DateHelper {
+  const DateHelper._();
+
+  static String formatDate(DateTime date) {
+    final year = date.year;
+    final month = date.month.toString().padLeft(2, '0');
+    final day = date.day.toString().padLeft(2, '0');
+    return '$year-$month-$day';
+  }
+}
