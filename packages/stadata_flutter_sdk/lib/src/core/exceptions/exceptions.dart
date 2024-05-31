3ba7e7eb9ce22924cf66af2881c391236a2cef7e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-05-31 21:17:40 +0800

    feat(exception): add statistic classification exception

--- packages/stadata_flutter_sdk/lib/src/core/exceptions/exceptions.dart
+++ packages/stadata_flutter_sdk/lib/src/core/exceptions/exceptions.dart
@@ -174,3 +174,10 @@ class UnitNotAvailableException extends UnitException {
     super.message = 'Unit not available!',
   });
 }
+
+class StatisticClassificationException extends StadataException {
+  const StatisticClassificationException({
+    super.message =
+        'There is something wrong with Statistic Classification data!',
+  });
+}
