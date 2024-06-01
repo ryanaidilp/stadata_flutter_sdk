1fe54f839135f42f53d1f68299c36a5c6cf1898e
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-06-01 11:36:01 +0800

    docs(entities): add doc comments to classifications entities

--- packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/entities/classification_item.dart
+++ packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/entities/classification_item.dart
@@ -1,10 +1,20 @@
 import 'package:stadata_flutter_sdk/src/core/base/base_entity.dart';
 
+/// Represents a single classification item entity.
+/// This class corresponds to individual items within a statistical classification.
+///
+/// See the [API Documentation](https://webapi.bps.go.id/documentation/#sc).
 class ClassificationItem extends BaseEntity {
+  /// Code of the classification item.
   final String code;
+
+  /// Title of the classification item.
   final String title;
+
+  /// Description of the classification item.
   final String description;
 
+  /// Creates a new instance of `ClassificationItem`.
   const ClassificationItem({
     required this.code,
     required this.title,
--- packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/entities/statistic_classification.dart
+++ packages/stadata_flutter_sdk/lib/src/features/statistical_classification/domain/entities/statistic_classification.dart
@@ -1,27 +1,69 @@
 import 'package:stadata_flutter_sdk/src/core/base/base_entity.dart';
 import 'package:stadata_flutter_sdk/src/features/features.dart';
 
+/// Represents a statistical classification entity.
+/// This class corresponds to the API data for statistical classifications.
+///
+/// See the [API Documentation](https://webapi.bps.go.id/documentation/#sc).
 class StatisticClassification extends BaseEntity {
+  /// Unique identifier for the statistical classification.
   final String id;
+
+  /// Type of the classification (optional).
   final ClassificationType? type;
+
+  /// Level of the classification (optional).
   final ClassificationLevel? level;
+
+  /// Source of the classification.
   final String source;
+
+  /// Title of the classification.
   final String title;
+
+  /// Description of the classification.
   final String description;
+
+  /// ISBN associated with the classification (optional).
   final String? isbn;
+
+  /// ISSN associated with the classification (optional).
   final String? issn;
+
+  /// Catalogue number of the classification (optional).
   final String? catalogueNumber;
+
+  /// Publication number of the classification (optional).
   final String? publicationNumber;
+
+  /// Last update date of the classification.
   final DateTime lastUpdate;
+
+  /// Release date of the classification.
   final DateTime releaseDate;
+
+  /// Location related to the classification (optional).
   final String? location;
+
+  /// URL for additional information or resources (optional).
   final String? url;
+
+  /// Metadata field (optional).
   final String? mfd;
+
+  /// List of previous classification items.
   final List<ClassificationItem> previous;
+
+  /// List of derived classification items.
   final List<ClassificationItem> derived;
+
+  /// A flag indicating some state of the classification.
   final bool flag;
+
+  /// List of tags associated with the classification.
   final List<String> tags;
 
+  /// Creates a new instance of `StatisticClassification`.
   const StatisticClassification({
     required this.id,
     required this.source,
