77331486d9dff15ccf903d177378ee73dbed93f3
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-08-01 09:11:15 +0800

    refactor(models): remove freezed & jsonserializable
    remove freezed & json_serializable from domain and infographic model

--- packages/stadata_flutter_sdk/lib/src/features/infographics/data/models/infographic_model.dart
+++ packages/stadata_flutter_sdk/lib/src/features/infographics/data/models/infographic_model.dart
@@ -1,32 +1,58 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/material.dart';
+import 'package:stadata_flutter_sdk/src/core/core.dart';
 import 'package:stadata_flutter_sdk/src/features/features.dart';
 
-part 'infographic_model.freezed.dart';
-part 'infographic_model.g.dart';
+const _idKey = 'inf_id';
+const _titleKey = 'title';
+const _imageKey = 'img';
+const _categoryKey = 'category';
+const _downloadKey = 'dl';
+const _descKey = 'desc';
 
-@freezed
-abstract class InfographicModel with _$InfographicModel {
-  factory InfographicModel({
-    @JsonKey(name: 'inf_id') required int id,
-    required String title,
-    @JsonKey(name: 'img') required String image,
-    required int category,
-    @JsonKey(name: 'dl') required String downloadUrl,
-    @JsonKey(name: 'desc') String? description,
-  }) = _InfographicModel;
-  factory InfographicModel.fromJson(Map<String, dynamic> json) =>
-      _$InfographicModelFromJson(json);
-}
+class InfographicModel extends Infographic {
+  const InfographicModel({
+    required super.id,
+    required super.title,
+    required super.image,
+    required super.category,
+    required super.downloadUrl,
+    super.description,
+  });
 
-extension InfographicModelX on InfographicModel {
-  Infographic toEntity() => Infographic(
-        id: id,
-        title: title,
-        image: image,
-        description: description,
-        category: category,
-        downloadUrl: downloadUrl,
+  InfographicModel copyWith({
+    int? id,
+    String? title,
+    String? image,
+    int? category,
+    String? downloadUrl,
+    ValueGetter<String?>? description,
+  }) =>
+      InfographicModel(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        image: image ?? this.image,
+        category: category ?? this.category,
+        downloadUrl: downloadUrl ?? this.downloadUrl,
+        description: description == null ? this.description : description(),
       );
+
+  factory InfographicModel.fromJson(JSON json) => InfographicModel(
+        id: json[_idKey] as int,
+        title: json[_titleKey] as String,
+        image: json[_imageKey] as String,
+        category: json[_categoryKey] as int,
+        downloadUrl: json[_downloadKey] as String,
+        description: json[_descKey] as String?,
+      );
+
+  JSON toJson() => {
+        _idKey: id,
+        _titleKey: title,
+        _imageKey: image,
+        _categoryKey: category,
+        _downloadKey: downloadUrl,
+        _descKey: description,
+      };
 }
