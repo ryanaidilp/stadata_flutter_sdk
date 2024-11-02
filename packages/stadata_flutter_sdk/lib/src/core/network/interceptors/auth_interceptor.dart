f22fb1e2b13a477f91333e200fd3fcfbbeda2d36
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-11-03 01:40:14 +0800

    feat(interceptors): create my own network interceptor

---
+++ packages/stadata_flutter_sdk/lib/src/core/network/interceptors/auth_interceptor.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:stadata_flutter_sdk/src/config/config.dart';
+import 'package:stadata_flutter_sdk/src/core/di/di.dart';
+import 'package:stadata_flutter_sdk/src/core/network/base_network_interceptor.dart';
+import 'package:stadata_flutter_sdk/src/core/network/request_data.dart';
+
+class AuthInterceptor extends BaseNetworkInterceptor {
+  final _apiConfig = injector.get<ApiConfig>();
+
+  @override
+  FutureOr<RequestData> onRequest(RequestData request) async {
+    final apiKey = _apiConfig.apiKey;
+
+    final newQueries = Map<String, dynamic>.from(request.uri.queryParameters);
+    newQueries['key'] = apiKey;
+
+    return super.onRequest(
+      request.copyWith(
+        uri: request.uri.replace(
+          queryParameters: newQueries,
+        ),
+      ),
+    );
+  }
+}
