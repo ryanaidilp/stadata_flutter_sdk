58d4d22278baee602c2b050c50d7b7fca5e0ecdc
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-11-03 01:39:48 +0800

    refactor(dio): remove dio related files

--- packages/stadata_flutter_sdk/lib/src/core/network/http/modules/stadata_list_http_module.dart
+++
@@ -1,6 +0,0 @@
-import 'package:stadata_flutter_sdk/src/core/core.dart';
-
-class StadataListHttpModule extends HttpModule {
-  StadataListHttpModule()
-      : super(injector.get<HttpClient>(instanceName: 'listClient'));
-}
