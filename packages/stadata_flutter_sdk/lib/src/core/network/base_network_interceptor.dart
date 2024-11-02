1a052b7bf2ecc0d727a12035980befb9bfb5f904
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-11-03 01:41:57 +0800

    style(code): run flutter format

--- packages/stadata_flutter_sdk/lib/src/core/network/base_network_interceptor.dart
+++ packages/stadata_flutter_sdk/lib/src/core/network/base_network_interceptor.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:stadata_flutter_sdk/src/core/network/request_data.dart';
 import 'package:stadata_flutter_sdk/src/core/network/response_data.dart';
 
-abstract  class BaseNetworkInterceptor {
+abstract class BaseNetworkInterceptor {
   FutureOr<RequestData> onRequest(RequestData request) => request;
   FutureOr<ResponseData> onResponse(ResponseData response) => response;
   FutureOr<ResponseData> onError(Object error, StackTrace stackTrace) =>
