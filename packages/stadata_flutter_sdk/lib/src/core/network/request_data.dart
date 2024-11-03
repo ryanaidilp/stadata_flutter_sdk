b3e5f9bb6e4727d08566a696d62d51fb5e6e2e05
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-11-04 03:26:01 +0800

    feat(network_client): create my own network client

---
+++ packages/stadata_flutter_sdk/lib/src/core/network/request_data.dart
@@ -0,0 +1,27 @@
+class RequestData {
+  final String method;
+  final Uri uri;
+  final Map<String, String> headers;
+
+  final dynamic body;
+
+  RequestData({
+    required this.method,
+    required this.uri,
+    required this.headers,
+    this.body,
+  });
+
+  RequestData copyWith({
+    String? method,
+    Uri? uri,
+    Map<String, String>? headers,
+    dynamic body,
+  }) =>
+      RequestData(
+        method: method ?? this.method,
+        uri: uri ?? this.uri,
+        headers: headers ?? this.headers,
+        body: body ?? this.body,
+      );
+}
