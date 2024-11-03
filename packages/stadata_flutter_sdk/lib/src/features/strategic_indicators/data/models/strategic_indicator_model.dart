88dd699bac0d118da6dd997e9b03cc6f93b61710
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2024-11-04 03:25:57 +0800

    refactor(models): strategic indicator clean up

--- packages/stadata_flutter_sdk/lib/src/features/strategic_indicators/data/models/strategic_indicator_model.dart
+++ packages/stadata_flutter_sdk/lib/src/features/strategic_indicators/data/models/strategic_indicator_model.dart
@@ -1,59 +1,89 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:stadata_flutter_sdk/src/core/core.dart';
 import 'package:stadata_flutter_sdk/src/features/features.dart';
 
-part 'strategic_indicator_model.freezed.dart';
-part 'strategic_indicator_model.g.dart';
+const _idKey = 'indicator_id';
+const _varKey = 'var';
+const _categoryKey = 'category';
+const _nameKey = 'name';
+const _titleKey = 'title';
+const _dataSourceKey = 'data_source';
+const _valueKey = 'value';
+const _hashIDKey = 'hash_id';
+const _periodKey = 'periode';
+const _unitKey = 'unit';
+const _subjectCsaIDKey = 'subject_csa';
 
-@freezed
-abstract class StrategicIndicatorModel with _$StrategicIndicatorModel {
-  factory StrategicIndicatorModel({
-    @JsonKey(name: 'indicator_id') required int id,
-    @JsonKey(name: 'var') required int variableID,
-    @JsonKey(name: 'category') required int categoryID,
-    required String name,
-    required String title,
-    required String dataSource,
-    required double value,
-    @JsonKey(name: 'hash_id') required String hashID,
-    @JsonKey(name: 'periode') required String period,
-    @Default('-') String unit,
-    @JsonKey(name: 'subject_csa') int? csaSubjectID,
-  }) = _StrategicIndicatorModel;
-  factory StrategicIndicatorModel.fromJson(Map<String, dynamic> json) =>
-      _$StrategicIndicatorModelFromJson(json);
+class StrategicIndicatorModel extends StrategicIndicator {
+  const StrategicIndicatorModel({
+    required super.id,
+    required super.name,
+    required super.title,
+    required super.variableID,
+    required super.categoryID,
+    required super.dataSource,
+    required super.value,
+    required super.hashID,
+    required super.period,
+    super.unit = '-',
+    super.csaSubjectID,
+  });
 
-  factory StrategicIndicatorModel.fromEntity(
-    StrategicIndicator strategicIndicator,
-  ) =>
+  factory StrategicIndicatorModel.fromJson(JSON json) =>
       StrategicIndicatorModel(
-        id: strategicIndicator.id,
-        categoryID: strategicIndicator.categoryID,
-        variableID: strategicIndicator.variableID,
-        csaSubjectID: strategicIndicator.csaSubjectID,
-        name: strategicIndicator.name,
-        title: strategicIndicator.title,
-        dataSource: strategicIndicator.dataSource,
-        value: strategicIndicator.value,
-        unit: strategicIndicator.unit,
-        hashID: strategicIndicator.hashID,
-        period: strategicIndicator.period,
+        id: json[_idKey] as int,
+        name: json[_nameKey] as String,
+        title: json[_titleKey] as String,
+        variableID: json[_varKey] as int,
+        categoryID: json[_categoryKey] as int,
+        dataSource: json[_dataSourceKey] as String,
+        value: json[_valueKey] as double,
+        hashID: json[_hashIDKey] as String,
+        period: json[_periodKey] as String,
+        unit: json[_unitKey] as String,
+        csaSubjectID: json[_subjectCsaIDKey] as int?,
       );
-}
 
-extension StrategicIndicatorModelX on StrategicIndicatorModel {
-  StrategicIndicator toEntity() => StrategicIndicator(
-        id: id,
-        title: title,
-        name: name,
-        dataSource: dataSource,
-        value: value,
-        unit: unit,
-        period: period,
-        hashID: hashID,
-        categoryID: categoryID,
-        variableID: variableID,
-        csaSubjectID: csaSubjectID,
+  JSON toJson() => {
+        _idKey: id,
+        _nameKey: name,
+        _titleKey: title,
+        _varKey: variableID,
+        _categoryKey: categoryID,
+        _dataSourceKey: dataSource,
+        _valueKey: value,
+        _hashIDKey: hashID,
+        _periodKey: period,
+        _unitKey: unit,
+        _subjectCsaIDKey: csaSubjectID,
+      };
+
+  StrategicIndicatorModel copyWith({
+    int? id,
+    String? name,
+    String? title,
+    int? variableID,
+    int? categoryID,
+    String? dataSource,
+    double? value,
+    String? hashID,
+    String? period,
+    String? unit,
+    ValueGetter<int?>? csaSubjectID,
+  }) =>
+      StrategicIndicatorModel(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        title: title ?? this.title,
+        variableID: variableID ?? this.variableID,
+        categoryID: categoryID ?? this.categoryID,
+        dataSource: dataSource ?? this.dataSource,
+        value: value ?? this.value,
+        hashID: hashID ?? this.hashID,
+        period: period ?? this.period,
+        unit: unit ?? this.unit,
+        csaSubjectID: csaSubjectID != null ? csaSubjectID() : this.csaSubjectID,
       );
 }
