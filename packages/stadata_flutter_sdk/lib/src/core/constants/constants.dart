fd6e3f4dcd5409dfb8a6f0200ba61ce618ebcfec
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-01-12 17:09:00 +0700

    feat(constant): create query param constant & injector constant

--- packages/stadata_flutter_sdk/lib/src/core/constants/constants.dart
+++ packages/stadata_flutter_sdk/lib/src/core/constants/constants.dart
@@ -2,3 +2,29 @@ class StorageConstant {
   /// Key identifier for api key in the local storage
   static const apiKey = 'api_key';
 }
+
+class QueryParamConstant {
+  const QueryParamConstant._();
+  static const id = 'id';
+  static const domain = 'domain';
+  static const page = 'page';
+  static const lang = 'lang';
+  static const prov = 'prov';
+  static const type = 'type';
+  static const newsCat = 'newscat';
+  static const month = 'month';
+  static const year = 'year';
+  static const keyword = 'keyword';
+  static const perPage = 'perpage';
+  static const level = 'level';
+  static const variable = 'var';
+  static const subjectCategory = 'subcat';
+  static const area = 'area';
+  static const subject = 'subject';
+}
+
+class InjectorConstant {
+  const InjectorConstant._();
+  static const listClient = 'listClient';
+  static const viewClient = 'viewClient';
+}
