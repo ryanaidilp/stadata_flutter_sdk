6f7c6f303b5db838c166b776e41d09adf7fb15e4
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-03-13 22:06:38 +0700

    feat([DOMAIN] repositories): create census repository

---
+++ packages/stadata_flutter_sdk/lib/src/features/census_data/domain/repositories/census_repository.dart
@@ -0,0 +1,7 @@
+import 'package:stadata_flutter_sdk/src/core/core.dart';
+import 'package:stadata_flutter_sdk/src/features/features.dart';
+import 'package:stadata_flutter_sdk/src/shared/shared.dart';
+
+abstract class CensusRepository {
+  Future<Result<Failure, ApiResponse<List<Census>>>> get();
+}
