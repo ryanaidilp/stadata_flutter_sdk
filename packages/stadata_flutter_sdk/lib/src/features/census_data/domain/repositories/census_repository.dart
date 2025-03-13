9f47c896e3e180b00d6db42cdabda728f465ef7c
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-03-14 04:15:53 +0700

    feat([DOMAIN] repositories): add method to get census topics

--- packages/stadata_flutter_sdk/lib/src/features/census_data/domain/repositories/census_repository.dart
+++ packages/stadata_flutter_sdk/lib/src/features/census_data/domain/repositories/census_repository.dart
@@ -4,4 +4,7 @@ import 'package:stadata_flutter_sdk/src/shared/shared.dart';
 
 abstract class CensusRepository {
   Future<Result<Failure, ApiResponse<List<Census>>>> get();
+  Future<Result<Failure, ApiResponse<List<CensusTopic>>>> getCensusTopics({
+    required String censusID,
+  });
 }
