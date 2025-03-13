a0b4463d6c39cdda976e22f6872bf16d22fdc605
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-03-13 22:41:07 +0700

    feat([DATA] repositories): create census repository implementation

---
+++ packages/stadata_flutter_sdk/lib/src/features/census_data/data/repositories/census_repository_impl.dart
@@ -0,0 +1,48 @@
+import 'package:meta/meta.dart';
+import 'package:stadata_flutter_sdk/src/core/core.dart';
+import 'package:stadata_flutter_sdk/src/features/features.dart';
+import 'package:stadata_flutter_sdk/src/shared/shared.dart';
+
+class CensusRepositoryImpl implements CensusRepository {
+  @internal
+  final Log logger;
+  @internal
+  final CensusRemoteDataSource remoteDataSource;
+
+  const CensusRepositoryImpl({
+    required this.logger,
+    required this.remoteDataSource,
+  });
+
+  @override
+  Future<Result<Failure, ApiResponse<List<Census>>>> get() async {
+    try {
+      final result = await remoteDataSource.get();
+
+      if (result.data == null) {
+        throw const CensusDataNotAvailableExcetion();
+      }
+
+      final data = result.data;
+
+      return Result.success(
+        ApiResponse(
+          status: result.status,
+          data: data,
+          dataAvailability: result.dataAvailability,
+          message: result.message,
+          pagination: result.pagination,
+        ),
+      );
+    } catch (e, s) {
+      await logger.console(
+        e.toString(),
+        error: e,
+        stackTrace: s,
+        type: LogType.error,
+      );
+
+      return Result.failure(CensusDataFailure(message: e.toString()));
+    }
+  }
+}
