fda0f7c37d2c94ed412b9508774fa51ac208b519
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-03-15 00:10:12 +0700

    test([REPOSITORIES]): write unit test for getCensusTopics

--- packages/stadata_flutter_sdk/test/src/features/census_data/data/repositories/census_repository_impl_test.dart
+++ packages/stadata_flutter_sdk/test/src/features/census_data/data/repositories/census_repository_impl_test.dart
@@ -14,9 +14,7 @@ class MockLog extends Mock implements Log {}
 void main() {
   late Log mockLog;
   late CensusRepository repository;
-  late ApiResponse<List<Census>> censuses;
   late CensusRemoteDataSource mockDataSource;
-  late ApiResponseModel<List<CensusModel>> successResponse;
 
   setUpAll(() {
     mockLog = MockLog();
@@ -27,32 +25,37 @@ void main() {
       logger: mockLog,
       remoteDataSource: mockDataSource,
     );
-
-    final json = jsonFromFixture(Fixture.census);
-
-    successResponse = ApiResponseModel<List<CensusModel>>.fromJson(json, (
-      json,
-    ) {
-      if (json is! List) {
-        return [];
-      }
-
-      return json.map((e) => CensusModel.fromJson(e as JSON)).toList();
-    });
-
-    final data = successResponse.data?.map((e) => e).toList();
-
-    censuses = ApiResponse(
-      status: successResponse.status,
-      data: data,
-      dataAvailability: successResponse.dataAvailability,
-      message: successResponse.message,
-      pagination: successResponse.pagination,
-    );
   });
 
   group('CensusRepositoryImpl', () {
     group('get()', () {
+      late ApiResponse<List<Census>> censuses;
+      late ApiResponseModel<List<CensusModel>> successResponse;
+
+      setUp(() {
+        final json = jsonFromFixture(Fixture.census);
+
+        successResponse = ApiResponseModel<List<CensusModel>>.fromJson(json, (
+          json,
+        ) {
+          if (json is! List) {
+            return [];
+          }
+
+          return json.map((e) => CensusModel.fromJson(e as JSON)).toList();
+        });
+
+        final data = successResponse.data?.map((e) => e).toList();
+
+        censuses = ApiResponse(
+          status: successResponse.status,
+          data: data,
+          dataAvailability: successResponse.dataAvailability,
+          message: successResponse.message,
+          pagination: successResponse.pagination,
+        );
+      });
+
       test('should return list of censuses if success', () async {
         // arrange
         when(
@@ -99,6 +102,98 @@ void main() {
           ),
         );
         verify(() => mockDataSource.get()).called(1);
+      });
+    });
+
+    group('getCensusTopics()', () {
+      const censusID = 'sp2020';
+      late ApiResponse<List<CensusTopic>> censusTopics;
+      late ApiResponseModel<List<CensusTopicModel>> successResponse;
+
+      setUp(() {
+        final json = jsonFromFixture(Fixture.censusTopic);
+
+        successResponse = ApiResponseModel<List<CensusTopicModel>>.fromJson(
+          json,
+          (json) {
+            if (json is! List) {
+              return [];
+            }
+
+            return json
+                .map((e) => CensusTopicModel.fromJson(e as JSON))
+                .toList();
+          },
+        );
+
+        final data = successResponse.data?.map((e) => e).toList();
+
+        censusTopics = ApiResponse(
+          status: successResponse.status,
+          data: data,
+          dataAvailability: successResponse.dataAvailability,
+          message: successResponse.message,
+          pagination: successResponse.pagination,
+        );
+      });
+
+      test('should return list of census topics if success', () async {
+        // arrange
+        when(
+          () =>
+              mockDataSource.getCensusTopics(censusID: any(named: 'censusID')),
+        ).thenAnswer((_) async => successResponse);
+
+        // act
+        final result = await repository.getCensusTopics(censusID: censusID);
+
+        // assert
+        expect(
+          result,
+          equals(
+            Result.success<Failure, ApiResponse<List<CensusTopic>>>(
+              censusTopics,
+            ),
+          ),
+        );
+        verify(
+          () => mockDataSource.getCensusTopics(censusID: censusID),
+        ).called(1);
+      });
+
+      test('should return Failure if failed', () async {
+        // arrange
+        when(
+          () =>
+              mockDataSource.getCensusTopics(censusID: any(named: 'censusID')),
+        ).thenThrow(const CensusTopicNotAvailableException());
+
+        when(
+          () => mockLog.console(
+            any(),
+            error: any<dynamic>(named: 'error'),
+            stackTrace: any(named: 'stackTrace'),
+            type: any(named: 'type'),
+          ),
+        ).thenAnswer((_) async => Future.value());
+
+        // act
+        final result = await repository.getCensusTopics(censusID: censusID);
+
+        // assert
+        expect(
+          result,
+          equals(
+            Result.failure<Failure, ApiResponse<List<CensusTopic>>>(
+              const CensusTopicFailure(
+                message: 'StadataException - Census Topic not available!',
+              ),
+            ),
+          ),
+        );
+        verify(
+          () => mockDataSource.getCensusTopics(censusID: censusID),
+        ).called(1);
       });
     });
   });
