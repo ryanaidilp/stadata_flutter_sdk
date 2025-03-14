01f7a475e0e8a3ab13470717e90e6fe1254efcbb
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-03-14 21:05:27 +0700

    test(fixture): create fixture for census topic

--- packages/stadata_flutter_sdk/test/fixtures/fixtures.dart
+++ packages/stadata_flutter_sdk/test/fixtures/fixtures.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 enum Fixture {
   census('census_list_fixture.json'),
+  censusTopic('census_topic_list_fixture.json'),
   domains('domain_list_fixture.json'),
   error('error_fixture.json'),
   infographics('infographic_list_fixture.json'),
