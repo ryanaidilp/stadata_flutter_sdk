36e40d4d0a68d8c7062813d6ac0a45a14595de11
ryanaidilp <ryanaidilp@users.noreply.github.com>
2025-07-06 05:43:53 +0000

    deploy: fa2a4ae90cf30010cd69d09f298b3ae89324ed20

--- assets/js/5e95c892.3f96cae4.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstadata_flutter_sdk=self.webpackChunkstadata_flutter_sdk||[]).push([[9647],{7121:(e,s,t)=>{t.r(s),t.d(s,{default:()=>n});t(6540);var a=t(8215),r=t(1003),u=t(7559),d=t(2831),c=t(781),l=t(4848);function n(e){return(0,l.jsx)(r.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,l.jsx)(c.A,{children:(0,d.v)(e.route.routes)})})}}}]);
